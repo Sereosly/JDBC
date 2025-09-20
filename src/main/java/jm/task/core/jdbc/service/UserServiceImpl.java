2556bc6b4a20c0a5290c71b359d34e58236b6be6
Aleksey/Bishopik <deadkniga@gmail.com>
2025-09-20 15:03:47 +0300

    delete useless сomments

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -14,7 +14,7 @@ public class UserServiceImpl implements UserService {
         this.connection = Util.getConnection();
     }
 
-//    @Override
+    @Override
     public void createUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS \"user\" (" +
                 "id BIGSERIAL PRIMARY KEY," +
@@ -30,7 +30,7 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-//    @Override
+    @Override
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS \"user\"";
         try (Statement statement = connection.createStatement()) {
@@ -41,7 +41,7 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-//    @Override
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO \"user\" (name, lastName, age) VALUES (?, ?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -55,7 +55,7 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-//    @Override
+    @Override
     public void removeUserById(long id) {
         String sql = "DELETE FROM \"user\" WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -71,7 +71,7 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-//    @Override
+    @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         String sql = "SELECT * FROM \"user\"";
@@ -91,7 +91,7 @@ public class UserServiceImpl implements UserService {
         return users;
     }
 
-//    @Override
+    @Override
     public void cleanUsersTable() {
         String sql = "TRUNCATE TABLE \"user\"";
         try (Statement statement = connection.createStatement()) {
